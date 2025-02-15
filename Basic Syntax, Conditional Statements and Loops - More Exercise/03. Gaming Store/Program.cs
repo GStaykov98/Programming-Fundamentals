87801e11cb6c01c825da0b1d1044048bf4e495f0
Gstaykov98 <g.staykov98@gmail.com>
2025-02-15 16:43:26 +0200

    All problems solved

---
+++ Basic Syntax, Conditional Statements and Loops - More Exercise/03. Gaming Store/Program.cs
@@ -0,0 +1,59 @@
+﻿double budget = double.Parse(Console.ReadLine());
+string input;
+double totalSpent = 0;
+
+while ((input = Console.ReadLine()) != "Game Time")
+{
+    string gameName = input;
+	double gamePrice = 0;
+
+	switch (gameName)
+	{
+		case "OutFall 4":
+			gamePrice = 39.99;
+			break;
+
+		case "CS: OG":
+			gamePrice = 15.99;
+			break;
+
+		case "Zplinter Zell":
+			gamePrice = 19.99;
+			break;
+
+		case "Honored 2":
+			gamePrice = 59.99;
+			break;
+
+		case "RoverWatch":
+			gamePrice = 29.99;
+			break;
+
+		case "RoverWatch Origins Edition":
+			gamePrice = 39.99;
+			break;
+
+        default:
+            Console.WriteLine("Not Found");
+			continue;
+	}
+
+	if (gamePrice > budget)
+	{
+        Console.WriteLine("Too Expensive");
+    }
+	else
+	{
+        Console.WriteLine($"Bought {gameName}");
+		totalSpent += gamePrice;
+		budget -= gamePrice;
+    }
+
+	if (budget == 0)
+	{
+        Console.WriteLine("Out of money!");
+		return;
+    }
+}
+
+Console.WriteLine($"Total spent: ${totalSpent:f2}. Remaining: ${budget:f2}");
---
+++ Basic Syntax, Conditional Statements and Loops - More Exercise/05. Messages/Program.cs
@@ -0,0 +1,30 @@
+﻿int n = int.Parse(Console.ReadLine());
+string output = string.Empty;
+
+for (int i = 0; i < n; i++)
+{
+    string input = Console.ReadLine();
+    int digit = int.Parse(input[0].ToString());
+    int length = input.Length;
+    int offset = (digit - 2) * 3;
+
+    if (digit == 8 || digit == 9)
+    {
+        offset += 1;
+    }
+
+    int index = offset + length - 1 + 97;
+
+    char currentChar = (char)index;
+
+    if (index == 91)
+    {
+        output += " ";
+    }
+    else
+    {
+        output += currentChar;
+    }
+}
+
+Console.WriteLine(output);

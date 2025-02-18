63e5c1431c8cd5cc3e121265a4621f0520640a81
Gstaykov98 <g.staykov98@gmail.com>
2025-02-18 19:18:50 +0200

    New solution added and some problems solved

---
+++ Data Types and Variables - Exercise/02. Sum Digits/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = int.Parse(Console.ReadLine());
+string numToString = n.ToString();
+long sum = 0;
+
+for (int i = 0; i < numToString.Length; i++)
+{
+    int currentDigit = int.Parse(numToString[i].ToString());
+    sum += currentDigit;
+}
+
+Console.WriteLine(sum);
---
+++ Data Types and Variables - Exercise/03. Elevator/Program.cs
@@ -0,0 +1,6 @@
+﻿int peopleCount = int.Parse(Console.ReadLine());
+int capacity = int.Parse(Console.ReadLine());
+
+int courses = (int)Math.Ceiling((double)peopleCount / capacity);
+
+Console.WriteLine(courses);

f01c9d14498463dd9d739249a1877a37edcfb895
Gstaykov98 <g.staykov98@gmail.com>
2025-02-22 19:15:03 +0200

    New solution created and solved some problems

---
+++ Data Types and Variables - More Exercise/02. From Left to The Right/Program.cs
@@ -0,0 +1,32 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    var input = Console.ReadLine().Split(" ");
+    long leftNum = long.Parse(input[0]);
+    long rightNum = long.Parse(input[1]);
+    int sum = 0;
+
+    if (leftNum > rightNum)
+    {
+        string numToString = leftNum.ToString();
+
+        for (int j = 0; j < numToString.Length; j++)
+        {
+            int currentDigit = int.Parse(numToString[j].ToString());
+            sum += currentDigit;
+        }
+    }
+    else
+    {
+        string numToString = rightNum.ToString();
+
+        for (int j = 0; j < numToString.Length; j++)
+        {
+            int currentDigit = int.Parse(numToString[j].ToString());
+            sum += currentDigit;
+        }
+    }
+
+    Console.WriteLine(sum);
+}
---
+++ Data Types and Variables - More Exercise/03. Floating Equality/Program.cs
@@ -0,0 +1,13 @@
+﻿double numA = double.Parse(Console.ReadLine());
+double numB = double.Parse(Console.ReadLine());
+double eps = 0.000001;
+double difference = Math.Abs(numA - numB);
+
+if (difference < eps)
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("False");
+}

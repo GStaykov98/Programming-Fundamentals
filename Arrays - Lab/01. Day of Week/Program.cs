fb6765eee5bf549cc6d7e86495b50a7a89ab621a
Gstaykov98 <g.staykov98@gmail.com>
2025-02-23 16:27:35 +0200

    New solution created and added some projects

---
+++ Arrays - Lab/01. Day of Week/Program.cs
@@ -0,0 +1,21 @@
+﻿int day = int.Parse(Console.ReadLine());
+
+string[] daysOfWeek =
+{
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+};
+
+if (day - 1 >= 0 && day - 1 <= 6)
+{
+    Console.WriteLine(daysOfWeek[day - 1]);
+}
+else
+{
+    Console.WriteLine("Invalid day!");
+}
---
+++ Arrays - Lab/04. Reverse Array of Strings/Program.cs
@@ -0,0 +1,8 @@
+﻿string[] input = Console.ReadLine().Split();
+
+input = input.Reverse().ToArray();
+
+for (int i = 0; i < input.Length; i++)
+{
+    Console.Write(input[i] + " ");
+}

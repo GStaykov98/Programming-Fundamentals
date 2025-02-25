9866eba7e41789835cf8b017297c3a3624255c74
Gstaykov98 <g.staykov98@gmail.com>
2025-02-25 14:55:41 +0200

    New solution created and added some files

---
+++ Arrays - Exercise/01. Train/Program.cs
@@ -0,0 +1,18 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] wagons = new int[n];
+int totalPeople = 0;
+
+for (int i = 0; i < n; i++)
+{
+    int currentPeople = int.Parse(Console.ReadLine());
+    totalPeople += currentPeople;
+    wagons[i] = currentPeople;
+}
+
+foreach (int wagon in wagons)
+{
+    Console.Write($"{wagon} ");
+}
+
+Console.WriteLine();
+Console.WriteLine(totalPeople);
---
+++ Arrays - Exercise/03. Zig-Zag Arrays/Program.cs
@@ -0,0 +1,33 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] arr1 = new int[n];
+int[] arr2 = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    int[] currentNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    int leftNum = currentNumbers[0];
+    int rightNum = currentNumbers[1];
+
+    if (i % 2 == 0)
+    {
+        arr1[i] = leftNum;
+        arr2[i] = rightNum;
+    }
+    else
+    {
+        arr1[i] = rightNum;
+        arr2[i] = leftNum;
+    }
+}
+
+foreach (int i in arr1)
+{
+    Console.Write(i + " ");
+}
+
+Console.WriteLine();
+
+foreach (int i in arr2)
+{
+    Console.Write(i + " ");
+}

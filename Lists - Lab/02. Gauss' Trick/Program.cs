8b890aeb4bf64c28b11972e645a0e0ab29213e24
Gstaykov98 <g.staykov98@gmail.com>
2025-03-02 18:13:01 +0200

    New solution created and added files

---
+++ Lists - Lab/02. Gauss' Trick/Program.cs
@@ -0,0 +1,10 @@
+﻿List<double> numbers = Console.ReadLine().Split().Select(double.Parse).ToList();
+int iterations = numbers.Count / 2;
+
+for (int i = 0; i < iterations; i++)
+{
+    numbers[i] += numbers[numbers.Count - 1];
+    numbers.RemoveAt(numbers.Count - 1);
+}
+
+Console.WriteLine(string.Join(" ", numbers));
---
+++ Lists - Lab/03. Merging Lists/Program.cs
@@ -0,0 +1,27 @@
+﻿List<int> firstList = Console.ReadLine().Split().Select(int.Parse).ToList();
+List<int> secondList = Console.ReadLine().Split().Select(int.Parse).ToList();
+var outputList = new List<int>();
+int shorterListCount = Math.Min(firstList.Count, secondList.Count);
+
+for (int i = 0; i < shorterListCount; i++)
+{
+    outputList.Add(firstList[i]);
+    outputList.Add(secondList[i]);
+}
+
+if (firstList.Count > secondList.Count)
+{
+    for (int i = secondList.Count; i < firstList.Count; i++)
+    {
+        outputList.Add(firstList[i]);
+    }
+}
+else if (secondList.Count > firstList.Count)
+{
+    for (int i = firstList.Count; i < secondList.Count; i++)
+    {
+        outputList.Add(secondList[i]);
+    }
+}
+
+Console.WriteLine(string.Join(" ", outputList));

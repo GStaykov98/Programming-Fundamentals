841deda72b40a04fd38304cd6cee560ac888c0f4
Gstaykov98 <g.staykov98@gmail.com>
2025-03-17 15:07:44 +0200

    All problems solved

---
+++ Associative Arrays - Lab/03. Word Synonyms/Program.cs
@@ -0,0 +1,22 @@
+﻿int n = int.Parse(Console.ReadLine());
+Dictionary<string, List<string>> synonyms = new();
+
+for (int i = 0; i < n; i++)
+{
+    string word = Console.ReadLine();
+    string synonym = Console.ReadLine();
+
+    if (!synonyms.ContainsKey(word))
+    {
+        synonyms.Add(word, new List<string> { synonym });
+    }
+    else
+    {
+        synonyms[word].Add(synonym);
+    }
+}
+
+foreach (var pair in synonyms)
+{
+    Console.WriteLine($"{pair.Key} - {string.Join(", ", pair.Value)}");
+}
---
+++ Associative Arrays - Lab/04. Word Filter/Program.cs
@@ -0,0 +1,6 @@
+﻿string[] input = Console.ReadLine().Split().Where(x => x.Length % 2 == 0).ToArray();
+
+foreach (var line in input)
+{
+    Console.WriteLine(line);
+}

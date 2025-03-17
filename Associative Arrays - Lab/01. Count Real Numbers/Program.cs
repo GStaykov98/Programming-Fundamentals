beb5beffac08a9c60b5e0b9d6fb80475a5f12b44
Gstaykov98 <g.staykov98@gmail.com>
2025-03-17 14:58:23 +0200

    New solution created and added files

---
+++ Associative Arrays - Lab/01. Count Real Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+SortedDictionary<int, int> keyValuePairs = new();
+
+foreach (var num in input)
+{
+    if (keyValuePairs.ContainsKey(num))
+    {
+        keyValuePairs[num]++;
+    }
+    else
+    {
+        keyValuePairs.Add(num, 1);
+    }
+}
+
+foreach (var pair in keyValuePairs)
+{
+    Console.WriteLine($"{pair.Key} -> {pair.Value}");
+}
---
+++ Associative Arrays - Lab/02. Odd Occurrences/Program.cs
@@ -0,0 +1,19 @@
+﻿string[] input = Console.ReadLine().Split();
+Dictionary<string, int> keyValuePairs = new();
+
+foreach (string element in input)
+{
+    if (keyValuePairs.ContainsKey(element.ToLower()))
+    {
+        keyValuePairs[element.ToLower()]++;
+    }
+    else
+    {
+        keyValuePairs.Add(element.ToLower(), 1);
+    }
+}
+
+foreach (var pair in keyValuePairs.Where(x => x.Value % 2 != 0))
+{
+    Console.Write($"{pair.Key} ");
+}

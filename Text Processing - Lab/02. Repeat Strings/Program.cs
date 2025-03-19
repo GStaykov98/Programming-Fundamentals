e765df01abdc171ef5907428dfd2aa2bf7cc2148
Gstaykov98 <g.staykov98@gmail.com>
2025-03-19 14:54:24 +0200

    New solution created and added files

---
+++ Text Processing - Lab/02. Repeat Strings/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+string[] input = Console.ReadLine().Split();
+StringBuilder sb = new StringBuilder();
+
+for (int i = 0; i < input.Length; i++)
+{
+    string currentWord = input[i];
+    int n = currentWord.Length;
+
+    for (int j = 0; j < n; j++)
+    {
+        sb.Append(currentWord);
+    }
+}
+
+Console.WriteLine(sb);
---
+++ Text Processing - Lab/03. Substring/Program.cs
@@ -0,0 +1,10 @@
+﻿string wordToRemove = Console.ReadLine();
+string input = Console.ReadLine();
+
+while (input.Contains(wordToRemove))
+{
+    int index = input.IndexOf(wordToRemove);
+    input = input.Remove(index, wordToRemove.Length);
+}
+
+Console.WriteLine(input);

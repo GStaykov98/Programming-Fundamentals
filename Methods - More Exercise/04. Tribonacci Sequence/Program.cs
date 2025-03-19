e6c2851fcd064dde65d9c1f0ec0ab5440053d484
Gstaykov98 <g.staykov98@gmail.com>
2025-03-19 15:58:17 +0200

    All problems solved

---
+++ Methods - More Exercise/04. Tribonacci Sequence/Program.cs
@@ -0,0 +1,29 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] numbers = new int[n];
+numbers = TribonacciSequence(n);
+
+Console.WriteLine(string.Join(" ", numbers));
+
+static int[] TribonacciSequence(int n)
+{
+    int[] output = new int[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        if (i < 2)
+        {
+            output[i] = 1;
+            continue;
+        }
+
+        if (i == 2)
+        {
+            output[i] = output[0] + output[1];
+            continue;
+        }
+
+        output[i] = output[i - 1] + output[i - 2] + output[i - 3];
+    }
+
+    return output;
+}
---
+++ Methods - More Exercise/05. Multiplication Sign/Program.cs
@@ -0,0 +1,65 @@
+﻿int n1 = int.Parse(Console.ReadLine());
+int n2 = int.Parse(Console.ReadLine());
+int n3 = int.Parse(Console.ReadLine());
+bool isPositive = true;
+
+if (!IsNumberPositive(n1))
+{
+    isPositive = ChangeResult(isPositive);
+}
+
+if (!IsNumberPositive(n2))
+{
+    isPositive = ChangeResult(isPositive);
+}
+
+if (!IsNumberPositive(n3))
+{
+    isPositive = ChangeResult(isPositive);
+}
+
+if (IsNumberZero(n1, n2, n3))
+{
+    Console.WriteLine("zero");
+}
+else
+{
+    if (isPositive)
+    {
+        Console.WriteLine("positive");
+    }
+    else if (!isPositive)
+    {
+        Console.WriteLine("negative");
+    }
+}
+
+
+static bool IsNumberPositive(int number)
+{
+    return number > 0;
+}
+
+static bool ChangeResult(bool currentResult)
+{
+    if (currentResult)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+static bool IsNumberZero(int n1, int n2, int n3)
+{
+    if (n1 == 0 ||
+        n2 == 0 ||
+        n3 == 0)
+    {
+        return true;
+    }
+     
+    return false;
+}

c7f6f77b04dcafd2e4dabb9b6e0a755550ceaf73
Gstaykov98 <g.staykov98@gmail.com>
2025-03-25 15:23:09 +0200

    All problems solved

---
+++ Lists - More Exercise/04. Mixed up Lists/Program.cs
@@ -0,0 +1,38 @@
+﻿List<int> firstList = Console.ReadLine().Split().Select(int.Parse).ToList();
+List<int> secondList = Console.ReadLine().Split().Select(int.Parse).ToList();
+List<int> outputList = new();
+int shorterList = Math.Min(firstList.Count, secondList.Count);
+
+for (int i = 0; i < shorterList; i++)
+{
+    outputList.Add(firstList[i]);
+    outputList.Add(secondList[secondList.Count - 1 - i]);
+}
+
+int minNumber = 0;
+int maxNumber = 0;
+
+if (firstList.Count > secondList.Count)
+{
+    minNumber = Math.Min(firstList[firstList.Count - 1], firstList[firstList.Count - 2]);
+    maxNumber = Math.Max(firstList[firstList.Count - 1], firstList[firstList.Count - 2]);
+}
+else
+{
+    minNumber = Math.Min(secondList[0], secondList[1]);
+    maxNumber = Math.Max(secondList[0], secondList[1]);
+}
+
+for (int i = 0; i < outputList.Count; i++)
+{
+    if (outputList[i] <= minNumber ||
+        outputList[i] >= maxNumber)
+    {
+        outputList.RemoveAt(i);
+        i--;
+    }
+}
+
+outputList.Sort();
+
+Console.WriteLine(string.Join(" ", outputList));
---
+++ Lists - More Exercise/05. Drum Set/Program.cs
@@ -0,0 +1,28 @@
+﻿double budget = double.Parse(Console.ReadLine());
+List<int> initalDrumSet = Console.ReadLine().Split().Select(int.Parse).ToList();
+List<int> currentDrumSet = new List<int>(initalDrumSet);
+string input;
+
+while ((input = Console.ReadLine()) != "Hit it again, Gabsy!")
+{
+    int power = int.Parse(input);
+
+	for (int i = 0; i < currentDrumSet.Count; i++)
+	{
+		currentDrumSet[i] -= power;
+
+		if (currentDrumSet[i] <= 0)
+		{
+			int drumPrice = initalDrumSet[i] * 3;
+
+			if (budget >= drumPrice)
+			{
+				currentDrumSet[i] = initalDrumSet[i];
+				budget -= drumPrice;
+			}
+		}
+	}
+}
+
+Console.WriteLine(string.Join(" ", currentDrumSet.Where(x => x > 0)));
+Console.WriteLine($"Gabsy has {budget:f2}lv.");

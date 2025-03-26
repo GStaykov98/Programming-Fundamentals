44f4efb1520934a1b6a01f32cb099b3a38fce9b1
Gstaykov98 <g.staykov98@gmail.com>
2025-03-26 18:37:22 +0200

    All problems solved

---
+++ Objects and Classes - More Exercise/04. Raw Data/Program.cs
@@ -0,0 +1,80 @@
+﻿int n = int.Parse(Console.ReadLine());
+List<Car> cars = new();
+
+for (int i = 0; i < n; i++)
+{
+    string[] tokens = Console.ReadLine().Split();
+    string model = tokens[0];
+    int engineSpeed = int.Parse(tokens[1]);
+    int enginePower = int.Parse(tokens[2]);
+    int cargoWeight = int.Parse(tokens[3]);
+    string cargoType = tokens[4];
+
+    Cargo currentCargo = new(cargoWeight, cargoType);
+    Engine currentEngine = new(engineSpeed, enginePower);
+    Car currentCar = new(model, currentEngine, currentCargo);
+
+    cars.Add(currentCar);
+}
+
+string neededType = Console.ReadLine();
+
+if (neededType == "fragile")
+{
+    foreach (var car in cars.Where(x => x.Cargo.Type == "fragile"))
+    {
+        if (car.Cargo.Weight < 1000)
+        {
+            Console.WriteLine(car.Model);
+        }
+    }
+}
+else if (neededType == "flamable")
+{
+    foreach (var car in cars.Where(x => x.Cargo.Type == "flamable"))
+    {
+        if (car.Engine.Power > 250)
+        {
+            Console.WriteLine(car.Model);
+        }
+    }
+}
+
+public class Car
+{
+    public Car(string model, Engine engine, Cargo cargo)
+    {
+        this.Model = model;
+        this.Engine = engine;
+        this.Cargo = cargo;
+    }
+
+    public string Model { get; set; }
+    public Engine Engine { get; set; }
+    public Cargo Cargo { get; set; }
+}
+
+public class Engine
+{
+    public Engine(int engineSpeed, int enginePower)
+    {
+        this.Speed = engineSpeed;
+        this.Power = enginePower;
+    }
+
+    public int Speed { get; set; }
+    public int Power { get; set; }
+}
+
+public class Cargo
+{
+    public Cargo(int cargoWeight, string cargoType)
+    {
+        this.Weight = cargoWeight;
+        this.Type = cargoType;
+    }
+
+    public int Weight { get; set; }
+    public string Type { get; set; }
+
+}
---
+++ Objects and Classes - More Exercise/05. Shopping Spree/Program.cs
@@ -0,0 +1,86 @@
+﻿string[] peopleArray = Console.ReadLine().Split(';');
+string[] productsArray = Console.ReadLine().Split(';');
+List<Person> people = new();
+List<Product> products = new();
+
+foreach (var person in peopleArray)
+{
+    if (string.IsNullOrEmpty(person)) continue;
+
+    string[] personTokens = person.Split('=');
+    string name = personTokens[0];
+    double money = double.Parse(personTokens[1]);
+
+    Person currentPerson = new(name, money);
+    people.Add(currentPerson);
+}
+
+foreach (var product in productsArray)
+{
+    if (string.IsNullOrEmpty(product)) continue;
+
+    string[] productTokent = product.Split('=');
+    string name = productTokent[0];
+    double cost = double.Parse(productTokent[1]);
+
+    Product currentProdut = new(name, cost);
+    products.Add(currentProdut);
+}
+
+string input;
+
+while ((input = Console.ReadLine()) != "END")
+{
+    string[] command = input.Split();
+    Person person = people.FirstOrDefault(x => x.Name == command[0]);
+    Product product = products.FirstOrDefault(x => x.Name == command[1]);
+
+    if (person.Money >= product.Cost)
+    {
+        person.Money -= product.Cost;
+        person.Products.Add(product);
+        Console.WriteLine($"{person.Name} bought {product.Name}");
+    }
+    else
+    {
+        Console.WriteLine($"{person.Name} can't afford {product.Name}");
+    }
+}
+
+foreach (var person in people)
+{
+    if (person.Products.Any())
+    {
+        Console.WriteLine($"{person.Name} - {string.Join(", ", person.Products.Select(x => x.Name))}");
+    }
+    else
+    {
+        Console.WriteLine($"{person.Name} - Nothing bought");
+    }
+}
+
+public class Person
+{
+    public Person(string name, double money)
+    {
+        this.Name = name;
+        this.Money = money;
+        this.Products = new List<Product>();
+    }
+
+    public string Name { get; set; }
+    public double Money { get; set; }
+    public List<Product> Products { get; set; }
+}
+
+public class Product
+{
+    public Product(string name, double cost)
+    {
+        this.Name = name;
+        this.Cost = cost;
+    }
+
+    public string Name { get; set; }
+    public double Cost { get; set; }
+}

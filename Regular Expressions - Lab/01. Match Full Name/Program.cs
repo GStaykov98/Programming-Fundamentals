520d92c8ddb3459a3d9b84093999be8be32cbd8b
Gstaykov98 <g.staykov98@gmail.com>
2025-03-27 17:35:53 +0200

    New solution created and added files

---
+++ Regular Expressions - Lab/01. Match Full Name/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+string names = Console.ReadLine();
+
+string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+";
+Regex regex = new Regex(pattern);
+MatchCollection matches = regex.Matches(names);
+
+foreach (Match match in matches)
+{
+    Console.Write(match.Value + " ");
+}

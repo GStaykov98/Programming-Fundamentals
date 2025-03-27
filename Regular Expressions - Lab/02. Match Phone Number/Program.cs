02ec6d16696e2ba3b33b9a5401930f6f8ce5381b
Gstaykov98 <g.staykov98@gmail.com>
2025-03-27 17:50:04 +0200

    Added files

---
+++ Regular Expressions - Lab/02. Match Phone Number/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+string input = Console.ReadLine();
+
+string pattern = @"\+359( |-)[2]\1\d{3}\1\d{4}\b";
+Regex regex = new Regex(pattern);
+var matches = regex.Matches(input);
+
+var phones = matches
+    .Cast<Match>()
+    .Select(a => a.Value.Trim())
+    .ToArray();
+
+Console.WriteLine(string.Join(", ", phones));

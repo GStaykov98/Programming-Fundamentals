828de790b3588fa7c32920d85db33ed1c161001c
Gstaykov98 <g.staykov98@gmail.com>
2025-03-29 15:35:46 +0200

    New solution created and added files

---
+++ Regular Expressions - Exercise/01. Furniture/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+Regex regex = new Regex(@">>(?<product>[A-z]+)<<(?<price>\d+(.*\d+))!(?<quantity>\d+)");
+double totalPrice = 0;
+List<string> products = new();
+string input;
+
+while ((input = Console.ReadLine()) != "Purchase")
+{
+    Match match = regex.Match(input);
+
+    if (match.Success)
+    {
+        string product = match.Groups["product"].Value;
+        double price = double.Parse(match.Groups["price"].Value);
+        int quantity = int.Parse(match.Groups["quantity"].Value);
+
+        products.Add(product);
+        totalPrice += price * quantity;
+    }
+}
+
+Console.WriteLine("Bought furniture:");
+foreach (string product in products)
+{
+    Console.WriteLine(product);
+}
+Console.WriteLine($"Total money spend: {totalPrice:f2}");

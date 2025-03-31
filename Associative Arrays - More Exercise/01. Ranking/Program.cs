d5c5301e07982989b0d042605b6857deda050c88
Gstaykov98 <g.staykov98@gmail.com>
2025-03-31 14:04:07 +0300

    New solution created and added files

---
+++ Associative Arrays - More Exercise/01. Ranking/Program.cs
@@ -0,0 +1,79 @@
+﻿Dictionary<string, string> contests = new();
+Dictionary<string, Dictionary<string, int>> students = new();
+string input;
+
+while ((input = Console.ReadLine()) != "end of contests")
+{
+    string[] tokens = input.Split(':');
+    string contestName = tokens[0];
+    string contestPassword = tokens[1];
+
+    contests.Add(contestName, contestPassword);
+}
+
+while ((input = Console.ReadLine()) != "end of submissions")
+{
+    string[] tokens = input.Split("=>");
+    string contestName = tokens[0];
+    string contestPassword = tokens[1];
+    string student = tokens[2];
+    int points = int.Parse(tokens[3]);
+
+    if (contests.ContainsKey(contestName))
+    {
+        if (contests[contestName] == contestPassword)
+        {
+            if (!students.ContainsKey(student))
+            {
+                students.Add(student, new Dictionary<string, int>());
+                students[student].Add(contestName, points);
+            }
+            else
+            {
+                if (!students[student].ContainsKey(contestName))
+                {
+                    students[student].Add(contestName, points);
+                }
+                else
+                {
+                    if (students[student][contestName] < points)
+                    {
+                        students[student][contestName] = points;
+                    }
+                }
+            }
+        }
+    }
+}
+
+string bestStudent = string.Empty;
+int mostPoints = 0;
+
+foreach (var student in students)
+{
+    int currentPoints = 0;
+
+    foreach (var contest in student.Value)
+    {
+        currentPoints += contest.Value;
+    }
+
+    if (currentPoints > mostPoints)
+    {
+        bestStudent = student.Key;
+        mostPoints = currentPoints;
+    }
+}
+
+Console.WriteLine($"Best candidate is {bestStudent} with total {mostPoints} points.");
+Console.WriteLine("Ranking:");
+
+foreach (var student in students.OrderBy(x => x.Key))
+{
+    Console.WriteLine(student.Key);
+
+    foreach (var contest in student.Value.OrderByDescending(x => x.Value))
+    {
+        Console.WriteLine($"#  {contest.Key} -> {contest.Value}");
+    }
+}
---
+++ Regular Expressions - Exercise/03. SoftUni Bar Income/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+Regex regex = new Regex(@"%(?<customer>[A-Z][a-z]+)%<(?<product>[A-Za-z]+)>\|(?<count>\d+)\|[^0-9]*(?<price>\d+(.*\d+)?)\$");
+double totalIncome = 0;
+string input;
+
+while ((input = Console.ReadLine()) != "end of shift")
+{
+    Match match = regex.Match(input);
+    
+    if (match.Success)
+    {
+        string name = match.Groups["customer"].Value;
+        string product = match.Groups["product"].Value;
+        int count = int.Parse(match.Groups["count"].Value);
+        double price = double.Parse(match.Groups["price"].Value);
+
+        totalIncome += price * count;
+        Console.WriteLine($"{name}: {product} - {price * count:f2}");
+    }
+}
+
+Console.WriteLine($"Total income: {totalIncome:f2}");
